d69617d6bf7a564d3ad77e8c3b5a4784c49067b2
Volodymyr Onyshchenko <volodymyr.onyshchenko.km13@gmail.com>
2022-12-06 01:02:00 +0200

    Step 3 done * Added tests for Step 3 * Added task implementation for Step 3 * Added tests coverage report for Step 3

--- src/main/java/oop/labs/lab1/StringCalculator.java
+++ src/main/java/oop/labs/lab1/StringCalculator.java
@@ -1,10 +1,15 @@
 package oop.labs.lab1;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class StringCalculator
 {
     public int add(String numbers) throws IllegalArgumentException
     {
         if (numbers.length() == 0) return 0;
+
+        var delimiters = new HashSet<>(Arrays.asList(',', '\n'));
 
         try
         {
@@ -14,7 +19,7 @@ public class StringCalculator
 
             while (i < numbers.length())
             {
-                while (i < numbers.length() && numbers.charAt(i) != ',') i++;
+                while (i < numbers.length() && !delimiters.contains(numbers.charAt(i))) i++;
 
                 if (i > 0 && i != numbers.length() - 1)
                 {
--- src/test/java/oop/labs/lab1/StringCalculatorTest.java
+++ src/test/java/oop/labs/lab1/StringCalculatorTest.java
@@ -42,4 +42,25 @@ public class StringCalculatorTest
         assertThat(testCalculator.add("1,2,3,4")).isEqualTo(10);
         assertThat(testCalculator.add("11,22,33")).isEqualTo(66);
     }
+
+    @Test
+    public void step_3_basic()
+    {
+        assertThat(testCalculator.add("1\n2")).isEqualTo(3);
+        assertThat(testCalculator.add("11\n22")).isEqualTo(33);
+        assertThat(testCalculator.add("1\n2,3\n4")).isEqualTo(10);
+        assertThat(testCalculator.add("11,22\n33")).isEqualTo(66);
+        assertThat(testCalculator.add("11\n22\n33\n44")).isEqualTo(110);
+    }
+
+    @Test
+    public void step_3_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("\n1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1,\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n,"))).isInstanceOf(IllegalArgumentException.class);
+    }
 }

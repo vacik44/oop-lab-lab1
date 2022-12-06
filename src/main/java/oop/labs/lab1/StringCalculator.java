b0ab2a0aa5c93abddc77a37377f31246b79e6666
Volodymyr Onyshchenko <119792242+vacik44@users.noreply.github.com>
2022-12-06 04:00:18 +0200

    Laboratory 1 done
    * Added tests for Step 1-9
    * Added task implementation for Step 1-9
    * Added tests coverage report for Step 1-9

---
+++ src/main/java/oop/labs/lab1/StringCalculator.java
@@ -0,0 +1,93 @@
+package oop.labs.lab1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class StringCalculator
+{
+    public int add(String numbers) throws IllegalArgumentException
+    {
+        if (numbers.length() == 0) return 0;
+
+        HashSet<String> delimiters = new HashSet<>(Arrays.asList(",", "\n"));
+        boolean rethrow = false;
+
+        try
+        {
+            int i = 0;
+            int border;
+
+            if (numbers.startsWith("//"))
+            {
+                if (numbers.charAt(2) == '[')
+                {
+                    i = 2;
+                    border = i + 1;
+
+                    while (numbers.charAt(i) == '[')
+                    {
+                        while (i < numbers.length() && numbers.charAt(i) != ']') i++;
+
+                        delimiters.add(numbers.substring(border, i));
+                        i++;
+                        border = i + 1;
+                    }
+
+                    if (numbers.charAt(i) != '\n') throw new IllegalArgumentException();
+                    i++;
+                }
+                else
+                {
+                    if (numbers.length() < 4 || numbers.charAt(3) != '\n') throw new IllegalArgumentException();
+
+                    delimiters.add(numbers.substring(2, 3));
+                    i = 4;
+                }
+
+                if (numbers.length() == i) return 0;
+            }
+
+            border = i;
+            int start = border;
+
+            int sum = 0;
+            ArrayList<Integer> negatives = new ArrayList<>();
+
+            while (i < numbers.length())
+            {
+                char c = numbers.charAt(i);
+
+                if (Character.isDigit(c) || c == '-')
+                {
+                    if (c == '-') i++;
+                    while (i < numbers.length() && Character.isDigit(numbers.charAt(i))) i++;
+
+                    int num = Integer.parseInt(numbers, border, i, 10);
+
+                    if (num < 0) negatives.add(num);
+                    if (negatives.isEmpty() && num <= 1000) sum += num;
+                }
+                else
+                {
+                    while (i < numbers.length() && (c = numbers.charAt(i)) != '-' && !Character.isDigit(c)) i++;
+
+                    if (border == start || i == numbers.length()) throw new IndexOutOfBoundsException();
+                    if (!delimiters.contains(numbers.substring(border, i))) throw new IllegalArgumentException();
+                }
+
+                border = i;
+            }
+
+            if (negatives.isEmpty()) return sum;
+
+            rethrow = true;
+            throw new IllegalArgumentException(String.format("Negatives not allowed (%s found).", negatives));
+        }
+        catch (IndexOutOfBoundsException | IllegalArgumentException ex)
+        {
+            if (rethrow) throw ex;
+            throw new IllegalArgumentException("Incorrect input format.");
+        }
+    }
+}
---
+++ src/test/java/oop/labs/lab1/StringCalculatorTest.java
@@ -0,0 +1,161 @@
+package oop.labs.lab1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class StringCalculatorTest
+{
+    private StringCalculator testCalculator;
+
+    @Before
+    public void prepare()
+    {
+        testCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void step_1_basic()
+    {
+        assertThat(testCalculator.add("")).isEqualTo(0);
+        assertThat(testCalculator.add("1")).isEqualTo(1);
+        assertThat(testCalculator.add("11")).isEqualTo(11);
+        assertThat(testCalculator.add("1,2")).isEqualTo(3);
+        assertThat(testCalculator.add("11,22")).isEqualTo(33);
+    }
+
+    @Test
+    public void step_1_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("@"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add(","))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add(",1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1,"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1,,"))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void step_2()
+    {
+        assertThat(testCalculator.add("1,2,3,4")).isEqualTo(10);
+        assertThat(testCalculator.add("11,22,33")).isEqualTo(66);
+    }
+
+    @Test
+    public void step_3_basic()
+    {
+        assertThat(testCalculator.add("1\n2")).isEqualTo(3);
+        assertThat(testCalculator.add("11\n22")).isEqualTo(33);
+        assertThat(testCalculator.add("1\n2,3\n4")).isEqualTo(10);
+        assertThat(testCalculator.add("11,22\n33")).isEqualTo(66);
+        assertThat(testCalculator.add("11\n22\n33\n44")).isEqualTo(110);
+    }
+
+    @Test
+    public void step_3_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("\n1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1,\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("1\n,"))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void step_4_basic()
+    {
+        assertThat(testCalculator.add("//;\n")).isEqualTo(0);
+        assertThat(testCalculator.add("//;\n11")).isEqualTo(11);
+        assertThat(testCalculator.add("//;\n1;2")).isEqualTo(3);
+        assertThat(testCalculator.add("//,\n1,2")).isEqualTo(3);
+        assertThat(testCalculator.add("//\n\n1\n2")).isEqualTo(3);
+        assertThat(testCalculator.add("//$\n11$22")).isEqualTo(33);
+        assertThat(testCalculator.add("//$\n1$2,3$4")).isEqualTo(10);
+        assertThat(testCalculator.add("//%\n11\n22%33")).isEqualTo(66);
+        assertThat(testCalculator.add("//%\n11%22%33%44")).isEqualTo(110);
+    }
+
+    @Test
+    public void step_4_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("//\n1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//;\n1*2"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//;\n;1;2"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//;;\n1;2"))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void step_5_basic()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("-1,2,3"))).hasMessage(String.format("Negatives not allowed (%s found).", List.of(-1)));
+        assertThat(catchThrowable(() -> testCalculator.add("1,-2,3"))).hasMessage(String.format("Negatives not allowed (%s found).", List.of(-2)));
+        assertThat(catchThrowable(() -> testCalculator.add("1,-2,-3"))).hasMessage(String.format("Negatives not allowed (%s found).", Arrays.asList(-2, -3)));
+        assertThat(catchThrowable(() -> testCalculator.add("-1,-2,-3"))).hasMessage(String.format("Negatives not allowed (%s found).", Arrays.asList(-1, -2, -3)));
+        assertThat(catchThrowable(() -> testCalculator.add("-11,-22,-33"))).hasMessage(String.format("Negatives not allowed (%s found).", Arrays.asList(-11, -22, -33)));
+    }
+
+    @Test
+    public void step_5_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("//;\n-11;-22;-33"))).hasMessage(String.format("Negatives not allowed (%s found).", Arrays.asList(-11, -22, -33)));
+        assertThat(catchThrowable(() -> testCalculator.add("//;\n11;-22\n33,-44"))).hasMessage(String.format("Negatives not allowed (%s found).", Arrays.asList(-22, -44)));
+        assertThat(catchThrowable(() -> testCalculator.add("//;\n11;-22\n33,-44\n"))).hasMessage("Incorrect input format.");
+    }
+
+    @Test
+    public void step_6()
+    {
+        assertThat(testCalculator.add("999,1000,1001")).isEqualTo(1999);
+        assertThat(testCalculator.add("1001,1000,999")).isEqualTo(1999);
+        assertThat(testCalculator.add("1001,999,20000")).isEqualTo(999);
+    }
+
+    @Test
+    public void step_7_basic()
+    {
+        assertThat(testCalculator.add("//[*]\n")).isEqualTo(0);
+        assertThat(testCalculator.add("//[*]\n1*2*3")).isEqualTo(6);
+        assertThat(testCalculator.add("//[*]\n1*2,3")).isEqualTo(6);
+        assertThat(testCalculator.add("//[*]\n11*22*33")).isEqualTo(66);
+        assertThat(testCalculator.add("//[*]\n11\n22*33")).isEqualTo(66);
+        assertThat(testCalculator.add("//[***]\n11***22***33")).isEqualTo(66);
+        assertThat(testCalculator.add("//[***]\n11***22***33")).isEqualTo(66);
+    }
+
+    @Test
+    public void step_7_extended()
+    {
+        assertThat(catchThrowable(() -> testCalculator.add("//[***\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***];\n"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]1,2"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n,"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n,1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n1,"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n***"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]1***2"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n***1"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n1***"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n1**2"))).isInstanceOf(IllegalArgumentException.class);
+        assertThat(catchThrowable(() -> testCalculator.add("//[***]\n1****2"))).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void step_8()
+    {
+        assertThat(testCalculator.add("//[+][*]\n1*2+3,4")).isEqualTo(10);
+        assertThat(testCalculator.add("//[+][*]\n11*22+33*44")).isEqualTo(110);
+        assertThat(testCalculator.add("//[+][*][/]\n11\n22+33*44/55")).isEqualTo(165);
+    }
+
+    @Test
+    public void step_9()
+    {
+        assertThat(testCalculator.add("//[***][*++][++][+][*]\n1*++2***3++4*5+6\n7,8")).isEqualTo(36);
+    }
+}
